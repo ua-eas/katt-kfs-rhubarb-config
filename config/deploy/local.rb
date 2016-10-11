fc032f042dc87e08c3ee249d0dca91da19d34ecc
Heather Lo <hlo@email.arizona.edu>
2016-10-11 14:32:51 -0700

    UAF-3050 Changing local deployment to port 22 to simplify deploying into Docker container within an EC2 instance that needs SSH access.

--- config/deploy/local.rb
+++ config/deploy/local.rb
@@ -1,6 +1,7 @@
 set :stage, :local
 
-role :app, %w{ssh-user@localhost:2222}
+role :app, %w{ssh-user@localhost:22}
 
 # Deploy from the develop branch
-set :branch, 'development'
+#set :branch, 'development'
+set :branch, 'UAF-3050'
